6465284b774a4063d086fc7c1fa70c0c0fab40b6
AustinDev16 <austin.blaser@gmail.com>
2016-09-02 13:32:46 -0600

    Set up views and subclassed view controllers and cell views.

---
+++ MovieSearch/MovieResultTableViewCell.swift
@@ -0,0 +1,19 @@
+//
+//  MovieResultTableViewCell.swift
+//  MovieSearch
+//
+//  Created by Austin Blaser on 9/2/16.
+//  Copyright © 2016 Austin Blaser. All rights reserved.
+//
+
+import UIKit
+
+class MovieResultTableViewCell: UITableViewCell {
+
+  
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var ratingLabel: UILabel!
+    @IBOutlet weak var descriptionLabel: UILabel!
+    @IBOutlet weak var movieImageView: UIImageView!
+
+}
